90ff65e5f7687750e7af9cf260e40df4997d1f0a
Yogitha.B.M <yogithabm75@gmail.com>
2026-07-18 12:48:26 +0530

    arrayproblems in JS

---
+++ arrayelements.js
@@ -0,0 +1,29 @@
+let nums=[1,2,3,4,5];
+function elements(){
+    console.log("Elemts of the array = ");
+    for(let num of nums){
+        console.log(num);
+    }
+}
+elements();
+
+function sum(){
+    nums.push(6);
+    let sum=0;
+    for(let num of nums){
+        sum+=num;
+    }
+    console.log("Sum of numbers : "+sum);
+}
+sum();
+
+function largest(){
+    let l=0;
+    for(let num of nums){
+        if (num[l]<num){
+            num[l]=num;
+        }
+    }
+    concole.log(num[l]);
+}
+largest();S
